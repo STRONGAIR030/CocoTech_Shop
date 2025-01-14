1b9ec0d53919c04590e196d1d9f5c132d34e488d
STRONGAIR030 <b89586639@gmail.com>
2025-01-14 11:49:52 +0800

    Refactory Adminorder for readability

--- src/Page/admin/AdminOrdersPage.jsx
+++ src/Page/admin/AdminOrdersPage.jsx
@@ -1,85 +1,85 @@
-import styled from "styled-components"
-import AdminLayout from "../../components/layout/AdminLayout"
-import StyledTableContainer from "../../components/common/StyledTableContainer"
-import { useContext, useEffect, useState } from "react"
-import AdminContext from "../../components/context/AdminContext"
-import { useNavigate } from "react-router"
-import StyledImgContainer from "../../components/common/StyledImgContainer"
+import styled from "styled-components";
+import AdminLayout from "../../components/layout/AdminLayout";
+import StyledTableContainer from "../../components/common/StyledTableContainer";
+import { useContext, useEffect, useState } from "react";
+import AdminContext from "../../components/context/AdminContext";
+import { useNavigate } from "react-router";
+import StyledImgContainer from "../../components/common/StyledImgContainer";
 
 const AdminOrdersPage = () => {
-    const {orderList, fetchOrderList, orderDataLoaded} = useContext(AdminContext);
-    const navigate = useNavigate();
+  const { orderList, fetchOrderList, orderDataLoaded } =
+    useContext(AdminContext);
+  const navigate = useNavigate();
 
-    const goAdminOrderById = (orderId) => {
-        navigate(`/admin/orders/${orderId}`)
-    } 
+  const goAdminOrderById = (orderId) => {
+    navigate(`/admin/orders/${orderId}`);
+  };
 
-    useEffect(() => {
-        fetchOrderList();
-    }, [])
+  useEffect(() => {
+    fetchOrderList();
+  }, []);
 
-    return (
-        <AdminLayout>
-            <StyledOrdersPage>
-                <h3>Order List</h3>
-                <StyledTableContainer $minWidth="800px">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>Order ID</th>
-                                <th>Customer</th>
-                                <th>Status</th>
-                                <th>Date</th>
-                                <th>Total</th>
-                                <th>Edit</th>
-                            </tr>
-                            {
-                                orderDataLoaded && orderList.length && orderList.map((order) => {
-                                    return (
-                                        <tr key={order.id}>
-                                            <td>{order.id}</td>
-                                            <td>{order.customerName}</td>
-                                            <td>{order.status}</td>
-                                            <td>{order.date}</td>
-                                            <td>{order.total}$</td>
-                                            <td>
-                                                <button 
-                                                onClick={() => {goAdminOrderById(order.id)}}
-                                                >
-                                                    <StyledImgContainer $imgUrl="/img/orderEdit.svg">
-                                                        <div/>
-                                                    </StyledImgContainer>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </StyledTableContainer>
-            </StyledOrdersPage>
-        </AdminLayout>
-    )
-}
+  return (
+    <AdminLayout>
+      <StyledOrdersPage>
+        <h3>Order List</h3>
+        <StyledTableContainer $minWidth="800px">
+          <table>
+            <tbody>
+              <tr>
+                <th>Order ID</th>
+                <th>Customer</th>
+                <th>Status</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Edit</th>
+              </tr>
+              {orderDataLoaded &&
+                orderList.length &&
+                orderList.map((order) => {
+                  return (
+                    <tr key={order.id}>
+                      <td>{order.id}</td>
+                      <td>{order.customerName}</td>
+                      <td>{order.status}</td>
+                      <td>{order.date}</td>
+                      <td>{order.total}$</td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            goAdminOrderById(order.id);
+                          }}
+                        >
+                          <StyledImgContainer $imgUrl="/img/orderEdit.svg">
+                            <div />
+                          </StyledImgContainer>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </StyledTableContainer>
+      </StyledOrdersPage>
+    </AdminLayout>
+  );
+};
 
-export default AdminOrdersPage
+export default AdminOrdersPage;
 
 const StyledOrdersPage = styled.div`
-    width: 100%;
-    padding: 16px;
+  width: 100%;
+  padding: 16px;
 
-    h3{
-        padding: 16px 0px;
-    }
+  h3 {
+    padding: 16px 0px;
+  }
 
-    td button {
-        width: 40px;
-        border-radius: 15px;
-        padding: 8px;
-        cursor: pointer;
-    }
-` 
-
-
-
+  td button {
+    width: 40px;
+    border-radius: 15px;
+    padding: 8px;
+    cursor: pointer;
+  }
+`;
